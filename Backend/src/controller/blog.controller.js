847594252b6008c3b4268896a21347c874fbcc18
om prakash sahu <114207732+doni-coder@users.noreply.github.com>
2024-07-05 13:01:06 +0530

    controller update-2

--- Backend/src/controller/blog.controller.js
+++ Backend/src/controller/blog.controller.js
@@ -37,21 +37,70 @@ const createBlog = asyncHandler(async (req, res) => {
 
   console.log(createdBlog._id);
 
-  await User.findByIdAndUpdate(
-    user._id,
-    {
-        $set:{
-            blogs:[createdBlog._id]
-        }
-    }
-  )
+  await User.findByIdAndUpdate(user._id, {
+    $set: {
+      blogs: [...user.blogs,createdBlog._id],
+    },
+  });
 
   return res
     .status(200)
     .json(new ApiResponse(200, createdBlog, "blog created"));
 });
 
+const editBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+  if (!id) {
+    throw new ApiError(400, "blog not found");
+  }
+  const blogImageLocal = req.file?.path;
+  const blogImage = await uploadToCloudinary(blogImageLocal);
 
-export {
-    createBlog
-}
+  const oldBlogImage = await Blog.findById(id);
+
+  const blog = await Blog.findByIdAndUpdate(
+    id,
+    {
+      $set: {
+        title,
+        description,
+        blogImage: blogImage?.url || oldBlogImage.blogImage,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  if (!blog) {
+    throw new ApiError(500, "unable to update blog");
+  }
+
+  return res.status(200).json(new ApiResponse(200, blog, "blog updated"));
+});
+
+const deleteBlog = asyncHandler(async (req,res)=>{
+  const {id} = req.params
+  const user = await User.findById(req.user?._id)
+  if (!user) {
+    throw new ApiError(400,"user not authenticated")
+  }
+
+  console.log(id);
+
+  const blog = await Blog.findByIdAndDelete(id)
+
+  if (!blog) {
+    throw new ApiError(500,"something went wrong")
+  }
+  const updatedBlogsArray = user.blogs.filter((blogId)=> blogId != id)
+  await User.findByIdAndUpdate(user._id,{
+    $set:{
+      blogs:updatedBlogsArray
+    }
+  })
+
+  return res.status(200).json(new ApiResponse(200,blog,"blog deleted"))
+})
+
+export { createBlog, editBlog,deleteBlog };
--- Backend/src/routes/blog.routes.js
+++ Backend/src/routes/blog.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import {createBlog} from "../controller/blog.controller.js"
+import {createBlog,editBlog,deleteBlog} from "../controller/blog.controller.js"
 import { upload } from "../middleware/multer.moddleware.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
 
 const router = Router()
 
 router.route("/create-blog").post(verifyJwt,upload.single("blogImage"),createBlog)
+router.route("/edit-blog/:id").put(verifyJwt,upload.single("blogImage"),editBlog)
+router.route("/delete-blog/:id").delete(verifyJwt,deleteBlog)
 
 export default router
